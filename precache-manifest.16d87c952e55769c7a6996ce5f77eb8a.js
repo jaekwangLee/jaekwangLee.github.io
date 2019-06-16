e550c74a06145f597ec12689a3ba2a8b656abcbe
jaekwanglee <bca1990@naver.com>
2019-06-16 17:50:14 +0900

    Updates

--- precache-manifest.16d87c952e55769c7a6996ce5f77eb8a.js
+++ precache-manifest.16d87c952e55769c7a6996ce5f77eb8a.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "354cc0e77d805e9c3311673455d0c4f2",
+    "revision": "a07b7caa1a3404c54369d2f05631e5d6",
     "url": "/index.html"
   },
   {
-    "revision": "aa5f82c0f54d63fb8d7a",
-    "url": "/static/css/main.086390cc.chunk.css"
+    "revision": "9e44a1388395b8d7bf82",
+    "url": "/static/css/main.9581fe64.chunk.css"
   },
   {
     "revision": "48f1c1b74e8dcbf4c9e9",
     "url": "/static/js/2.4fad3ce5.chunk.js"
   },
   {
-    "revision": "aa5f82c0f54d63fb8d7a",
+    "revision": "9e44a1388395b8d7bf82",
     "url": "/static/js/main.86979a92.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.798475cf046d414bc975a1f6970914e0.js"
+  "/precache-manifest.16d87c952e55769c7a6996ce5f77eb8a.js"
 );
 
 self.addEventListener('message', (event) => {
