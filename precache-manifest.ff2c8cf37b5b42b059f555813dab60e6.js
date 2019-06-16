16b319a59a7478ab945542de990f2a7859ff1fff
jaekwanglee <bca1990@naver.com>
2019-06-16 11:50:08 +0900

    Updates

---
+++ precache-manifest.ff2c8cf37b5b42b059f555813dab60e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b651aa86f6e066e71edca15f875c34cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "604a3a752944e50becad",
+    "url": "/static/css/main.1231a5ef.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "604a3a752944e50becad",
+    "url": "/static/js/main.94a44534.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
